b213d85bc77a7b065ffcf2c89735f73548c3853f
priyashah13 <priyashah2020@u.northwestern.edu>
2019-02-22 14:52:13 -0600

    DFS passing 5/8

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -2,6 +2,7 @@ from game_master import GameMaster
 from read import *
 from util import *
 
+
 class TowerOfHanoiGame(GameMaster):
 
     def __init__(self):
@@ -33,7 +34,46 @@ class TowerOfHanoiGame(GameMaster):
         Returns:
             A Tuple of Tuples that represent the game state
         """
-        ### student code goes here
+        ## student code goes here
+
+        onpeg1 = self.kb.kb_ask(parse_input("fact: (on ?X peg1)"))
+        onpeg2 = self.kb.kb_ask(parse_input("fact: (on ?X peg2)"))
+        onpeg3 = self.kb.kb_ask(parse_input("fact: (on ?X peg3)"))
+
+        peg1tuple = []
+        peg2tuple = []
+        peg3tuple = []
+
+        if onpeg1:
+            for i in range(len(onpeg1)):
+                for binding in onpeg1[i].bindings:
+                    string = str(binding.constant)
+                    index = len(string)
+                    peg1tuple.append(int(string[index - 1]))
+            peg1tuple.sort()
+
+        if onpeg2:
+            for i in range(len(onpeg2)):
+                for binding in onpeg2[i].bindings:
+                    string = str(binding.constant)
+                    index = len(string)
+                    peg2tuple.append(int(string[index - 1]))
+            peg2tuple.sort()
+
+        if onpeg3:
+            for i in range(len(onpeg3)):
+                for binding in onpeg3[i].bindings:
+                    string = str(binding.constant)
+                    index = len(string)
+                    peg3tuple.append(int(string[index - 1]))
+            peg3tuple.sort()
+
+        gameState = (tuple(peg1tuple), tuple(peg2tuple), tuple(peg3tuple))
+
+        # pdb.set_trace()
+        print(gameState)
+        return gameState
+
         pass
 
     def makeMove(self, movable_statement):
@@ -53,7 +93,61 @@ class TowerOfHanoiGame(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+
+        term = movable_statement.terms
+        disk = str(term[0])
+        oldp = str(term[1])
+        newp = str(term[2])
+
+        # retract the Top of the old peg
+        retractTop = "fact: (Top " + disk + " " + oldp + ")"
+        self.kb.kb_retract(parse_input(retractTop))
+
+        under = "fact: (onTop " + disk + " ?X)"
+        underAns = self.kb.kb_ask(parse_input(under))
+        emptyTop = "fact: (empty " + oldp + ")"
+
+        if underAns:
+            print("under moving disk")
+            print(underAns[0].bindings[0].constant)
+            retractonTop = "fact: (onTop " + disk + " " + str(underAns[0].bindings[0].constant) + ")"
+            self.kb.kb_retract(parse_input(retractonTop))
+            addNewTop = "fact: (Top " + str(underAns[0].bindings[0].constant) + " " + oldp + ")"
+            self.kb.kb_assert(parse_input(addNewTop))
+        else:
+            print("under moving disk")
+            print(False)
+            self.kb.kb_assert(parse_input(emptyTop))
+
+        retractOn = "fact: (on " + disk + " " + oldp + ")"
+        self.kb.kb_retract(parse_input(retractOn))
+
+        # new peg updates
+        # set new top
+        under2 = "fact: (Top ?X " + newp + ")"
+        under2Ans = self.kb.kb_ask(parse_input(under2))
+
+        # if there was a top, retract it before adding new top
+        if under2Ans:
+            print("top of target peg:")
+            print(under2Ans[0].bindings[0].constant)
+            retractTop2 = "fact: (Top " + str(under2Ans[0].bindings[0].constant) + " " + newp + ")"
+            self.kb.kb_retract(parse_input(retractTop2))
+            addonTopNew = "fact: (onTop " + disk + " " + str(under2Ans[0].bindings[0].constant) + ")"
+            self.kb.kb_assert(parse_input(addonTopNew))
+        else:
+            print("top of target peg:")
+            print(False)
+            emptyremove = "fact: (empty " + newp + ")"
+            self.kb.kb_retract(parse_input(emptyremove))
+
+        # add disk to new peg
+        addOn = "fact: (on " + disk + " " + newp + ")"
+        self.kb.kb_assert(parse_input(addOn))
+
+        addTop = "fact: (Top " + disk + " " + newp + ")"
+        self.kb.kb_assert(parse_input(addTop))
+
 
     def reverseMove(self, movable_statement):
         """
@@ -100,6 +194,52 @@ class Puzzle8Game(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### Student code goes here
+
+        row1 = self.kb.kb_ask(parse_input("fact: (location ?X ?pos pos1)"))
+        row2 = self.kb.kb_ask(parse_input("fact: (location ?X ?pos pos2)"))
+        row3 = self.kb.kb_ask(parse_input("fact: (location ?X ?pos pos3)"))
+
+        row1tuple = [-1, -1, -1]
+        row2tuple = [-1, -1, -1]
+        row3tuple = [-1, -1, -1]
+
+        for i in range(len(row1)):
+            tile = str(row1[i].bindings[0].constant)
+            pos = str(row1[i].bindings[1].constant)
+            tilenum = tile[len(tile) - 1]
+            posnum = int(pos[len(pos) - 1])
+
+            if tilenum == "y":
+                row1tuple[posnum - 1] = -1
+            else:
+                row1tuple[posnum - 1] = int(tilenum)
+
+        for i in range(len(row2)):
+            tile = str(row2[i].bindings[0].constant)
+            pos = str(row2[i].bindings[1].constant)
+            tilenum = tile[len(tile) - 1]
+            posnum = int(pos[len(pos) - 1])
+
+            if tilenum == "y":
+                row2tuple[posnum - 1] = -1
+            else:
+                row2tuple[posnum - 1] = int(tilenum)
+
+        for i in range(len(row3)):
+            tile = str(row3[i].bindings[0].constant)
+            pos = str(row3[i].bindings[1].constant)
+            tilenum = tile[len(tile) - 1]
+            posnum = int(pos[len(pos) - 1])
+
+            if tilenum == "y":
+                row3tuple[posnum - 1] = -1
+            else:
+                row3tuple[posnum - 1] = int(tilenum)
+
+        gameState = (tuple(row1tuple), tuple(row2tuple), tuple(row3tuple))
+
+        return gameState
+
         pass
 
     def makeMove(self, movable_statement):
@@ -119,6 +259,25 @@ class Puzzle8Game(GameMaster):
             None
         """
         ### Student code goes here
+
+        term = movable_statement.terms
+
+        tile_name = str(term[0])
+        posxi = str(term[1])
+        posyi = str(term[2])
+        posxt = str(term[3])
+        posyt = str(term[4])
+
+        retract = "fact: (location " + tile_name + " " + posxi + " " + posyi + ")"
+        add = "fact: (location " + tile_name + " " + posxt + " " + posyt + ")"
+        retractempty = "fact: (location empty " + posxt + " " + posyt + ")"
+        addempty = "fact: (location empty" + " " + posxi + " " + posyi + ")"
+
+        self.kb.kb_retract(parse_input(retract))
+        self.kb.kb_retract(parse_input(retractempty))
+        self.kb.kb_assert(parse_input(add))
+        self.kb.kb_assert(parse_input(addempty))
+
         pass
 
     def reverseMove(self, movable_statement):
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -1,5 +1,6 @@
 
 from solver import *
+import pdb
 
 class SolverDFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
@@ -18,8 +19,66 @@ class SolverDFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
-        ### Student code goes here
-        return True
+        # Student code goes here
+
+        curr = self.currentState
+
+        # check the base case
+        # if the victory condition is met, just return True, and done.
+        if curr.state == self.victoryCondition:
+            return True
+
+        # however, if the victory condition is not met, go through the DFS
+        else:
+            # get all movables that are possible form the current statement
+            movables = self.gm.getMovables()
+            # print(self.currentState.depth, movables)
+            if not curr.children and movables:
+                # when there are no more children and there are movables,
+                # for every possible, explore that move by making the move, saving the game state as a child
+                # then reverse the move
+                for move in movables:
+                    self.gm.makeMove(move)
+                    # increase depth by one because we are at the next depth
+                    d = curr.depth+1
+                    next = GameState(self.gm.getGameState(), d, move)
+                    # print(next.state)
+                    curr.children.append(next)
+                    next.parent = curr
+                    self.gm.reverseMove(move)
+        # self.unwalk()
+        while len(curr.children) > curr.nextChildToVisit:
+            # do the following while there are still children left to visit
+            # go to the next child in the list of children
+            next = curr.children[curr.nextChildToVisit]
+            # increment the index
+            curr.nextChildToVisit += 1
+            # if the child was not previously visited,
+            # walk the child using DFS
+            if next not in self.visited:
+                self.walk(next)
+                # self.visited[newState] = True
+                if self.currentState.state == self.victoryCondition:
+                    return True
+                break
+            self.unwalk()
+        return False
+
+    def walk(self, next):
+        # first mark the child as visited
+        self.visited[next] = True
+        # make the move that got us to the next
+        # then update the current state with the child's state
+        self.gm.makeMove(next.requiredMovable)
+        self.currentState = next
+
+    def unwalk(self):
+        # while self.currentState exists and all the children have been visited
+        while self.currentState and self.currentState.nextChildToVisit == len(self.currentState.children):
+            # keep going up the tree
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            # set the parent as the current state so we can keep moving up
+            self.currentState = self.currentState.parent
 
 
 class SolverBFS(UninformedSolver):
@@ -39,5 +98,5 @@ class SolverBFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
-        ### Student code goes here
+        # Student code goes here
         return True
